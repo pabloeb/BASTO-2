9bf4611fca1a973d4bbb97736936b5afc2636346
Ricardo <rikigermanramos@gmail.com>
2023-04-14 14:59:59 -0300

    ejemplo de reload modulo

--- ETL/main.py
+++ ETL/main.py
@@ -1,5 +1,8 @@
 from infra.connection_db import save_tranformation, read_mongo
-
+from importlib import reload
+import helpers.datarowdata_transfo # cargamos modulo
+reload(helpers.datarowdata_transfo) # recargar modulo si cambian sus funciones
+from helpers.datarowdata_transfo import flat_datarows #importamos sus funciones
 
 #example
 #df = read_mongo('animals')

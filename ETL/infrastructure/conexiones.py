16970e972487d1ff2389f347b5a04d2de941eb9f
Ricardo <rikigermanramos@gmail.com>
2023-05-06 21:14:53 -0300

    modifico conexiones etl

--- ETL/infrastructure/conexiones.py
+++ ETL/infrastructure/conexiones.py
@@ -6,6 +6,23 @@ import os
 # Para conexion con API de Google Drive:
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from pymongo import MongoClient # instalar pymongo
+
+def read_mongo(collection, query={}):
+    """ Make a query to the specific DB and Collection and get a dataframe"""
+
+     #es necesario python 3.6 o mayor 
+    mongo_uri = 'mongodb+srv://rikigerman:Q34UGooQa74zviWb@cluster1.m0ifpk2.mongodb.net/?retryWrites=true&w=majority'
+    conn = MongoClient(mongo_uri)
+    db = conn['test']
+    cursor = db[collection].find(query)
+    # Expand the cursor and construct the DataFrame
+    df =  pd.DataFrame(list(cursor))
+    df.index = df['_id'].apply(lambda x: str(x))
+    df.drop('_id',errors='ignore',inplace=True, axis=1)
+    df.index.name = None   
+    conn.close()
+    return df   # Read from Mongo and Store into DataFrame 
 
 
 def bson_to_dataframe(file_name):

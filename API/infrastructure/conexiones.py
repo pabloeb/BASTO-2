3bc572de5b05de56440434e3f190d319013edaac
Ricardo <rikigermanramos@gmail.com>
2023-05-10 19:06:55 -0300

    añado conexiones a transformations

--- API/infrastructure/conexiones.py
+++ API/infrastructure/conexiones.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+
+def get_hechos_df():
+    """obtiene dataframe de transformaciones de dataset tabla_hechos"""
+    return pd.read_csv('../ETL/transformations/tabla_hechos_final.csv')
+
+def get_ith():
+    """obtiene dataframe de transformaciones de dataset ith"""
+    return pd.read_csv('../ETL/transformations/tabla_ith.csv.csv')
+
 
